1b289ecdf579cf8276e9f24b0838a4233c5f69ce
duongital <duongital@gmail.com>
2024-09-06 14:27:08 +0700

    chore: fix build failed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -36,9 +36,14 @@ export default function Footer() {
         <p className="text-gray9 text-center">
           © <b>2022 Youngerist</b> - Vì sự tự tin cho vẻ đẹp của bạn
         </p>
-        <p className='text-center'>
+        <p className="text-center">
           Đồng sáng lập:
-          <a className='underline ml-2' target="_blank" href="https://penguin.vn/">
+          <a
+            className="underline ml-2"
+            target="_blank"
+            rel="noreferrer"
+            href="https://penguin.vn/"
+          >
             penguin.vn
           </a>
         </p>
