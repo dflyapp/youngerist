c69e26fc5c027305ac197b6d52539eb65b4fea15
duongital <duongital@gmail.com>
2025-03-10 16:25:00 +0700

    test process env

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -4,6 +4,8 @@ export default function Page() {
   return (
     <div>
       <h1>Page</h1>
+      <p>process env {process.env.BETTER_AUTH_SECRET}</p>
+      <p>process env {process.env.DATABASE_URL}</p>
       <Button />
     </div>
   )
