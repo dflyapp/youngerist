f9f3d5923d025ab50f8f18cf8c2cac5284d735cf
duongital <duongital@gmail.com>
2025-03-10 17:17:33 +0700

    move variable to client side

--- app/test/Button.tsx
+++ app/test/Button.tsx
@@ -1,36 +1,45 @@
-"use client"
+'use client'
 
 import { authClient } from '../../src/lib/client'
 
 export default function Button() {
-    return (
-        <button
-                onClick={async () => {
-                  const { data, error } = await authClient.signUp.email(
-                    {
-                      email: 'duongital@gmail.com', // user email address
-                      password: 'thanhtraTRA123!', // user password -> min 8 characters by default
-                      name: 'Duong', // user display name
-                    //   image: '', // user image url (optional)
-                      callbackURL: '/', // a url to redirect to after the user verifies their email (optional)
-                    },
-                    {
-                      onRequest: (ctx) => {
-                        //show loading
-                      },
-                      onSuccess: (ctx) => {
-                        //redirect to the dashboard or sign in page
-                      },
-                      onError: (ctx) => {
-                        // display the error message
-                        alert(ctx.error.message)
-                      },
-                    }
-                  )
-                  console.log({ data, error })
-                }}
-              >
-                Register
-              </button>
-    )
+  return (
+    <>
+      <p>process env BETTER_AUTH_SECRET {process.env.BETTER_AUTH_SECRET}</p>
+      <p>process env DATABASE_URL {process.env.DATABASE_URL}</p>
+      <p>process env BETTER_AUTH_URL {process.env.BETTER_AUTH_URL}</p>
+      <p>
+        process env NEXT_PUBLIC_SUPABASE_URL{' '}
+        {process.env.NEXT_PUBLIC_SUPABASE_URL}
+      </p>
+      <button
+        onClick={async () => {
+          const { data, error } = await authClient.signUp.email(
+            {
+              email: 'duongital@gmail.com', // user email address
+              password: 'thanhtraTRA123!', // user password -> min 8 characters by default
+              name: 'Duong', // user display name
+              //   image: '', // user image url (optional)
+              callbackURL: '/', // a url to redirect to after the user verifies their email (optional)
+            },
+            {
+              onRequest: (ctx) => {
+                //show loading
+              },
+              onSuccess: (ctx) => {
+                //redirect to the dashboard or sign in page
+              },
+              onError: (ctx) => {
+                // display the error message
+                alert(ctx.error.message)
+              },
+            }
+          )
+          console.log({ data, error })
+        }}
+      >
+        Register
+      </button>
+    </>
+  )
 }
--- app/test/page.tsx
+++ app/test/page.tsx
@@ -4,10 +4,7 @@ export default function Page() {
   return (
     <div>
       <h1>Page</h1>
-      <p>process env {process.env.BETTER_AUTH_SECRET}</p>
-      <p>process env {process.env.DATABASE_URL}</p>
-      <p>process env {process.env.BETTER_AUTH_URL}</p>
-      <p>process env NEXT_PUBLIC_SUPABASE_URL {process.env.NEXT_PUBLIC_SUPABASE_URL}</p>
+      
       <Button />
     </div>
   )
