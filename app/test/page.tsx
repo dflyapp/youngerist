adf51259d4c5d0c77112bec0ba7120430d104053
duongital <duongital@gmail.com>
2025-03-10 17:56:25 +0700

    add more logs

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,9 +1,16 @@
 import Button from './Button'
 
-export default function Page() {
+export default async function Page() {
   console.log('BETTER_AUTH_SECRET', process.env.BETTER_AUTH_SECRET)
   console.log('BETTER_AUTH_URL', process.env.BETTER_AUTH_URL)
   console.log('NEXT_PUBLIC_SUPABASE_URL', process.env.NEXT_PUBLIC_SUPABASE_URL)
+  console.log('DATABASE_URL', process.env.DATABASE_URL)
+
+  console.log(JSON.stringify({
+    severity: "INFO",
+    message: "✅ Cloud Run Structured Log",
+    timestamp: new Date().toISOString(),
+  }));
 
   return (
     <div>
