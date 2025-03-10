fd7ccfc7def6081acdcd020ecc8ae41bd68af56c
duongital <duongital@gmail.com>
2025-03-10 11:35:45 +0700

    add better auth

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,10 +1,12 @@
 import { betterAuth } from 'better-auth'
 import { drizzleAdapter } from 'better-auth/adapters/drizzle'
 import { db } from '../../db/db' // your drizzle instance
+import { user, session, account, verification } from '../../db/auth-schema'
 
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
     provider: 'pg', // or "mysql", "sqlite"
+    schema: { user, account, session, verification },
   }),
   emailAndPassword: {
     enabled: true,
